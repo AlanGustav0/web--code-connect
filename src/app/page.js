d9dd0c1349a68fcfc34c38193a9148ad9b00c58a
AlanGustav0 <alagustavo946@gmail.com>
2025-05-08 15:15:52 -0300

    feature: Implaments pagination searchParams

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,12 @@
 import { CardPost } from "@/components/CardPost";
 import logger from "@/log";
 import styles from "./page.module.css";
+import Link from "next/link";
 
-const post = {
-  id: 1,
-  cover:
-    "https://raw.githubusercontent.com/viniciosneves/code-connect-assets/main/posts/introducao-ao-react.png",
-  title: "Introdução ao React",
-  slug: "introducao-ao-react",
-  body: "Neste post, vamos explorar os conceitos básicos do React, uma biblioteca JavaScript para construir interfaces de usuário. Vamos cobrir componentes, JSX e estados.",
-  markdown:
-    "```javascript\nfunction HelloComponent() {\n  return <h1>Hello, world!</h1>;\n}\n```",
-  author: {
-    id: 101,
-    name: "Ana Beatriz",
-    username: "anabeatriz_dev",
-    avatar:
-      "https://raw.githubusercontent.com/viniciosneves/code-connect-assets/main/authors/anabeatriz_dev.png",
-  },
-};
-
-async function getAllPosts() {
-  const response = await fetch("http://localhost:3042/posts").catch((error) => {
+async function getAllPosts(page) {
+  const response = await fetch(
+    `http://localhost:3042/posts?_page=${page}&_per_page=6`
+  ).catch((error) => {
     logger.error("Ops, não foi possível recuperar os posts");
     return null;
   });
@@ -34,13 +19,22 @@ async function getAllPosts() {
   return response.json();
 }
 
-export default async function Home() {
-  const posts = await getAllPosts();
+export default async function Home({ searchParams }) {
+  const currentPage = searchParams.page || 1;
+  const { data: posts, prev, next } = await getAllPosts(currentPage);
   return (
-    <main className={styles.page}>
-      {posts.map((post) => (
-        <CardPost post={post} />
-      ))}
-    </main>
+    <>
+      <div className={styles.page}>
+        <main className={styles.main}>
+          {posts.map((post) => (
+            <CardPost key={post.id} post={post} />
+          ))}
+        </main>
+        <div className={styles.links}>
+          {prev && <Link href={`/?page=${prev}`}>Página Anterior</Link>}
+          {next && <Link href={`/?page=${next}`}>Próxima Página</Link>}
+        </div>
+      </div>
+    </>
   );
 }
